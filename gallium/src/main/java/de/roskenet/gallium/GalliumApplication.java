36444de341ffa25065d5a1ef248c6503b5a5de67
Felix Roske <felix@zalando.de>
2025-04-16 19:26:17 +0200

    Add gallium Spring Shell

---
+++ gallium/src/main/java/de/roskenet/gallium/GalliumApplication.java
@@ -0,0 +1,13 @@
+package de.roskenet.gallium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalliumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalliumApplication.class, args);
+	}
+
+}

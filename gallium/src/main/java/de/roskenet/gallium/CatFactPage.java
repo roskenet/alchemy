d7e94c9daadc8b8775ff433be10f80d8d4efc4da
Felix Roske <felix@zalando.de>
2025-04-17 00:05:11 +0200

    Get a random cat fact

--- gallium/src/main/java/de/roskenet/gallium/CatFactPage.java
+++
@@ -1,6 +0,0 @@
-package de.roskenet.gallium;
-
-import java.util.List;
-
-public record CatFactPage(List<CatFact> data) {
-}
--- gallium/src/main/java/de/roskenet/gallium/MyCommands.java
+++ gallium/src/main/java/de/roskenet/gallium/MyCommands.java
@@ -9,7 +9,7 @@ import org.springframework.shell.standard.ShellOption;
 @ShellComponent
 public class MyCommands {
 
-	@ShellMethod
+	@ShellMethod(value = "Prints a greeting to the user")
 	public String helloWorld(
 			@ShellOption(help = "The name you want to greet!", defaultValue = "spring") String name,
 			@ShellOption(help = "The age of this person", defaultValue = "42") int age
@@ -17,18 +17,15 @@ public class MyCommands {
 		return "Hello " + name + "!\nYou are " + age + " years old.";
 	}
 
-	@ShellMethod
+	@ShellMethod(value = "Prints a cat fact")
 	public String callCatFacts(
-			@ShellOption(help = "The number of cat facts you want to get", defaultValue = "1") int count
+			@ShellOption(help = "The max length of the fact you accept", defaultValue = "255") int maxlength
 	) {
 		var builder = new RestTemplateBuilder().rootUri("https://catfact.ninja")
 				.messageConverters(new MappingJackson2HttpMessageConverter());
 
-		var catFactPage = builder.build().getForObject("/facts?limit=" + count, CatFactPage.class);
+		var catFactPage = builder.build().getForObject("/fact?max_length=" + maxlength, CatFact.class);
 
-		return catFactPage.data().stream()
-				.map(CatFact::fact)
-				.reduce((a, b) -> a + "\n" + b)
-				.orElse("");
+		return catFactPage.fact();
 	}
 }

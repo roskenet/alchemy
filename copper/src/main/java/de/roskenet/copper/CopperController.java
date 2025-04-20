1f7c983dff1a05fce527e2b63cfdf61570660b9e
Felix Roske <felix@zalando.de>
2025-04-20 18:55:02 +0200

    New directory structure

--- copper/src/main/java/de/roskenet/copper/CopperController.java
+++ copper/src/main/java/de/roskenet/copper/CopperController.java
@@ -1,10 +1,12 @@
 package de.roskenet.copper;
 
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ManagedResource(objectName = "de.roskenet.copper:type=CopperController")
 public class CopperController {
 
     @GetMapping("/hello/{name}")

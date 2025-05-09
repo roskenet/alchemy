5574717b3242b55cb6ba3ecab2766e83906d10f7
Felix Roske <felix@zalando.de>
2025-05-09 13:01:10 +0200

    Add substring kata

---
+++ 06_kata/kata-java/src/main/java/de/roskenet/StringCounter.java
@@ -0,0 +1,40 @@
+package de.roskenet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCounter {
+
+    public static int countLongestSubstring(String s) {
+
+        boolean hasUppercase = false;
+        int longestSubstring = 0;
+        int currentCounter=0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if(Character.isUpperCase(s.charAt(i))) {
+               hasUppercase = true;
+               currentCounter++;
+               continue;
+            }
+            if(Character.isDigit(s.charAt(i))) {
+                if(hasUppercase) {
+                    longestSubstring = currentCounter;
+                }
+                currentCounter = 0;
+                hasUppercase = false;
+            }
+            currentCounter++;
+        }
+
+        return longestSubstring;
+    }
+
+    @Test
+    void countLongestSubstringTest() {
+        assertThat(countLongestSubstring("abcabcbb")).isEqualTo(0);
+        assertThat(countLongestSubstring("abcAbc0bb")).isEqualTo(6);
+        assertThat(countLongestSubstring("AA4b")).isEqualTo(2);
+    }
+}

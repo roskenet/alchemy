9d38c1a8310fbf5a3168fe1eefc70a9443756005
Felix Roske <felix@zalando.de>
2025-05-25 22:36:04 +0200

    Add demo for arbitrary precision classes

---
+++ 01_languages/java/java-features/src/main/java/felix/demo/precision/PrecisionDemo.java
@@ -0,0 +1,13 @@
+package felix.demo.precision;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class PrecisionDemo {
+
+    public static void main(String[] args) {
+        BigInteger one = BigInteger.ONE;
+
+        BigDecimal two = BigDecimal.TWO;
+    }
+}

a6e65bd3848644cda920e142361c1b01cfd5347c
Felix Roske <felix@zalando.de>
2025-06-19 18:59:05 +0200

    Switch demo to Spring Boot 3.5

--- 04_demos/querydsl-demo/src/test/java/playground/querydsl/EmbeddedDatasourceConfig.java
+++ 04_demos/querydsl-demo/src/test/java/playground/querydsl/EmbeddedDatasourceConfig.java
@@ -1,14 +1,13 @@
 package playground.querydsl;
 
 import io.zonky.test.db.postgres.embedded.EmbeddedPostgres;
-import java.io.IOException;
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.io.IOException;
 
 @Configuration
-@Profile("test")
 public class EmbeddedDatasourceConfig {
     @Bean
     public DataSource dataSource() throws IOException {

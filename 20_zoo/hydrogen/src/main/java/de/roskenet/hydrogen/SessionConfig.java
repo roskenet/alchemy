a8082635e7706198fae2201787b2490c525d3beb
Felix Roske <felix@zalando.de>
2025-06-21 23:44:30 +0200

    Add Spring Boot session

---
+++ 20_zoo/hydrogen/src/main/java/de/roskenet/hydrogen/SessionConfig.java
@@ -0,0 +1,29 @@
+package de.roskenet.hydrogen;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+/**
+ * Configuration for Spring Session with Redis.
+ * <p>
+ * This class configures Spring Session to use Redis as the session store.
+ * The actual connection properties are defined in application.properties.
+ */
+@Configuration
+@EnableRedisHttpSession
+public class SessionConfig {
+
+    /**
+     * Creates a Redis connection factory using Lettuce.
+     * <p>
+     * Connection details are automatically configured from application.properties.
+     *
+     * @return the Redis connection factory
+     */
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+}

3ef55ae9a8a4dddd73d610b6a530e47365aa9832
Felix Roske <felix@zalando.de>
2025-07-05 20:24:47 +0200

    Use special petunias.read / write scopes

--- 20_poc/petunia/axillaris/docs/token.sh
+++ 20_poc/petunia/axillaris/docs/token.sh
@@ -3,4 +3,6 @@
 export TOKEN=`curl -X POST http://keycloak/realms/petunia/protocol/openid-connect/token \
   -d "grant_type=client_credentials" \
   -d "client_id=villadiana" \
-  -d "client_secret=6w8HibfpYVEQIgMTKAFXFPMC8NgaOHBU" | jq -r .access_token`
+  -d "client_secret=6w8HibfpYVEQIgMTKAFXFPMC8NgaOHBU" \
+  -d "scope=petunias.read petunias.write" | jq -r .access_token`
+

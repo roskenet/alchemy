a773738f0376b3c1ad1d5c84bf7cb9536955af75
Felix Roske <felix@zalando.de>
2025-07-08 23:49:42 +0200

    Enable tests again

--- 20_poc/petunia/villadiana/src/test/java/de/petunia/villadiana/PetuniaControllerTest.java
+++ 20_poc/petunia/villadiana/src/test/java/de/petunia/villadiana/PetuniaControllerTest.java
@@ -1,6 +1,5 @@
 package de.petunia.villadiana;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Disabled("Until we mock the axillaris client.")
 public class PetuniaControllerTest {
 
     @Autowired

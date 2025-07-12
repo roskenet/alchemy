4601aabcfbcf4ab532f1d994ed04545e375d4038
Felix Roske <felix@zalando.de>
2025-07-12 13:15:29 +0200

    Open security for websocket example

--- 20_petunia/services/villadiana/src/main/java/de/petunia/villadiana/config/SecurityConfig.java
+++ 20_petunia/services/villadiana/src/main/java/de/petunia/villadiana/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/api/test-message**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(oauth -> oauth
                         .loginPage("/oauth2/authorization/keycloak")

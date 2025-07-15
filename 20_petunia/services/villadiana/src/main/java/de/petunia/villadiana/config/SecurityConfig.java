f26c6bcc4cdb95fffb369a21371c57f21b9e1732
Felix Roske <felix@zalando.de>
2025-07-15 12:54:32 +0200

    Open login endpoints

--- 20_petunia/services/villadiana/src/main/java/de/petunia/villadiana/config/SecurityConfig.java
+++ 20_petunia/services/villadiana/src/main/java/de/petunia/villadiana/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/actuator/**", "/oauth2/**", "/logout", "/login").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(oauth -> oauth
                         .loginPage("/oauth2/authorization/keycloak")

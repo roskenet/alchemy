63091b9ee84975e13aad3290489566e3a3916b45
Felix Roske <felix@zalando.de>
2026-01-17 10:27:17 +0100

    Sync Sat Jan 17 10:27:48 CET 2026

---
+++ 01_languages/c_cpp/PaktLearnC/Ch1_HelloWorld/hello1.c
@@ -0,0 +1,12 @@
+/*
+ * hello1.c
+ * The absolute KillerApp.
+ * by felix@roskenet.de
+ * created 2026-01-15
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+}
--- 01_languages/c_cpp/hello_c/main.c
+++ 01_languages/c_cpp/hello_c/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+char* get_greeting() {
+    return "Hello, World!";
+}
+
 int main(void) {
-    printf("Hello, World!\n");
+    printf("%s\n", get_greeting());
     return 0;
 }

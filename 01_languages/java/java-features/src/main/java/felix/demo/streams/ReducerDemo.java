dafaa0187d919e0612e1b30539d97c5876b3b84a
Felix Roske <felix@zalando.de>
2026-02-08 22:35:24 +0100

    It's Java25

--- 01_languages/java/java-features/src/main/java/felix/demo/streams/ReducerDemo.java
+++ 01_languages/java/java-features/src/main/java/felix/demo/streams/ReducerDemo.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ReducerDemo {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         List<String> majorMarathons = Arrays.asList("Berlin", "Tokyo", "New York", "Boston", "Chicao", "Sydney", "Cape Town");
 
         String collect = majorMarathons.stream().collect(Collectors.joining(", "));

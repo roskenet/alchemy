a6d0642a8d52b30ddc1b15372c6f5bc93b1d9ed3
Felix Roske <felix@zalando.de>
2026-02-11 23:35:41 +0100

    Add a decompile section

---
+++ 06_kata/kata-java/src/main/java/unsorted/DecompileDemo.java
@@ -0,0 +1,10 @@
+package unsorted;
+
+public class DecompileDemo {
+    private static String myString = "Hello World!";
+    private static int myInt = 42;
+
+    public static void main(String[] args) {
+        System.out.println(myString + " The answer is: " + myInt);
+    }
+}

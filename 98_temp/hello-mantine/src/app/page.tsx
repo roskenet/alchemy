59680b59974f1763efeb26b0d650aeab1379146a
Felix Roske <felix@roskenet.de>
2026-06-16 23:31:15 +0200

    Add page.tsx

--- 98_temp/hello-mantine/src/app/page.tsx
+++ 98_temp/hello-mantine/src/app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useState } from "react";
 import { Button, Center, Stack, Text, Title } from "@mantine/core";
 
+
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  function doSomething() {
+      setCount(count + 1);
+      console.log("count", count);
+  }
+
   return (
     <Center h="100vh">
       <Stack align="center" gap="lg">
@@ -8,7 +19,14 @@ export default function Home() {
         <Text c="dimmed" size="lg">
           Built with Next.js and Mantine
         </Text>
-        <Button size="md" variant="filled">
+        <Text size="xl" fw={700}>
+          Count: {count}
+        </Text>
+        <Button
+          size="md"
+          variant="filled"
+          onClick={() => {doSomething(); console.log("clicked");}}
+        >
           Get Started
         </Button>
       </Stack>

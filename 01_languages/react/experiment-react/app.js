05c3da9079c416c69a9eb618fc08293501db5413
Felix Roske <felix@roskenet.de>
2026-08-16 15:31:24 +0200

    Add a go example

--- 01_languages/react/experiment-react/app.js
+++ 01_languages/react/experiment-react/app.js
@@ -3,14 +3,15 @@ function App() {
     <div>
       <h1>{hello()}</h1>
       <p>Dies ist ein kleines Experiment mit JSX!</p>
+      <p>Ich bin ein Paragraph!</p>
     </div>
   );
 }
 
 function hello() {
+    // debugger
     return "Hello World!";
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
